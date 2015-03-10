b5b0bd144c75e69758986050c66a6791b274afdc
Gerco van Heerdt <gercovanheerdt@gmail.com>
2015-03-10 11:37:28 +0100

    Oh oh

--- source/Main.hs
+++ source/Main.hs
@@ -8,6 +8,12 @@ import SPL.Printer
 parseSPL :: String -> [Stmt]
 parseSPL = parse $ pPre >@ parse pSPL
 
+test :: String -> Bool
+test s = p == (parseSPL . prettyPrint) p
+    where
+    p = parseSPL s
+
 main = do
     s <- getContents
-    putStr $ prettyPrint $ parseSPL s
+    putStr . prettyPrint $ parseSPL s
+    print . test $ s
