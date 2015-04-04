d07ec3fb6e83d45570d8523315f7faf3ed7096d2
Tom Sanders <mail@tomsanders.nl>
2015-04-05 01:38:31 +0200

    Work in progress

--- source/SPL/Sequencer.hs
+++ source/SPL/Sequencer.hs
@@ -1,6 +1,35 @@
 module SPL.Sequencer where
+import SPL.Algebra
 import SPL.Typer
 
 genSeq :: [StmtT] -> String
-genSeq _ = "halt"
+genSeq ss = unlines $ concatMap genSeqStmt ss
+-- genSeq (FunCallT "print" [EIntT x TInt]) = unlines ["ldc " ++ x, "trap 0"]
 
+genSeqStmt s = case s of
+    StmtsT ss -> concatMap genSeqStmt ss
+    VarDeclT _ _ _ -> ["halt"] -- not yet implemented
+    FunDeclT _ _ _ _ -> ["halt"] -- not yet implemented
+    FunCallT "print" [e] -> case getType e of
+        TInt -> genSeqExp e ++ ["trap 0"]
+        TChar -> genSeqExp e ++ ["trap 1"]
+    FunCallT _ _ -> ["halt"] -- not yet implemented
+    ReturnT _ -> ["halt"] -- not yet implemented
+    AssignT _ _ _ -> ["halt"] -- not yet implemented
+    IfT _ _ _ -> ["halt"] -- not yet implemented
+    WhileT _ _ -> ["halt"] -- not yet implemented
+
+genSeqExp e = case e of
+    EIntT x TInt -> ["ldc " ++ show x]
+    EBoolT x TBool -> case x of
+        True -> ["ldc 1"]
+        False -> ["ldc 0"]
+    ECharT x TChar -> ["ldc " ++ show x]
+    ENilT _ -> ["nop"]
+    ETupleT e1 e2 _ -> genSeqExp e1 ++ genSeqExp e2
+    EIdT _ _ _ -> ["halt"] -- not yet implemented
+    EFunCallT _ _ _ -> ["halt"] -- not yet implemented
+    EOp1T op e _ -> genSeqExp e ++ genSeqOp op
+    EOp2T op e1 e2 _ -> genSeqExp e1 ++ genSeqOp e2
+
+genSeqOp _ = ["halt"]
