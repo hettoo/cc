df64c3ce3d73039bc53f88b7a486388ee9a63746
Gerco van Heerdt <gercovanheerdt@gmail.com>
2015-04-23 11:10:30 +0200

    Use the Context module

--- source/SPL/Sequencer.hs
+++ source/SPL/Sequencer.hs
@@ -2,18 +2,17 @@ module SPL.Sequencer where
 import SPL.Algebra
 import SPL.Typer
 import State
+import Context
 import Control.Monad
-import Data.Map (Map)
-import qualified Data.Map as M
 
 type Instruction = String
 
-type FunId = String
 type VarId = String
+type FunId = String
 type Label = String
 type HeapAddr = Int
-type FunTab = Map FunId Label
-type VarTab = Map VarId HeapAddr
+type FunTab = Context Label
+type VarTab = Context HeapAddr
 
 type Sequencer = State (FunTab, VarTab) [Instruction]
 
@@ -21,25 +20,25 @@ seqf :: (a -> Sequencer) -> [a] -> Sequencer
 seqf f = foldl (\s a -> s >> f a) (return [])
 
 seqOutput :: [StmtT] -> String
-seqOutput ss = unlines $ seqf seqStmt ss >!> (M.empty, M.empty)
+seqOutput ss = unlines $ seqf seqStmt ss >!> (cnew, cnew)
 
 seqStmt :: StmtT -> Sequencer
 seqStmt s = case s of
     StmtsT ss -> seqf seqStmt ss
-    VarDeclT t id e -> halt "not yet implemented" 
-    FunDeclT _ _ _ _ -> halt "not yet implemented" 
+    VarDeclT t id e -> halt "not yet implemented"
+    FunDeclT _ _ _ _ -> halt "not yet implemented"
     FunCallT "print" [e] -> do
         s <- seqExp e
         return $ s ++ case getType e of
             TInt -> ["trap 0"]
             TChar -> ["trap 1"]
-    FunCallT _ _ -> halt "not yet implemented" 
-    ReturnT _ -> halt "not yet implemented" 
-    AssignT _ _ _ -> halt "not yet implemented" 
-    IfT _ _ _ -> halt "not yet implemented" 
-    WhileT _ _ -> halt "not yet implemented" 
+    FunCallT _ _ -> halt "not yet implemented"
+    ReturnT _ -> halt "not yet implemented"
+    AssignT _ _ _ -> halt "not yet implemented"
+    IfT _ _ _ -> halt "not yet implemented"
+    WhileT _ _ -> halt "not yet implemented"
 
-seqExp :: ExpT -> Sequencer 
+seqExp :: ExpT -> Sequencer
 seqExp e = case e of
     EIntT x TInt -> return ["ldc " ++ show x]
     EBoolT x TBool -> case x of
@@ -48,10 +47,10 @@ seqExp e = case e of
     ECharT x TChar -> return ["ldc " ++ show x]
     ENilT _ -> return ["nop"]
     ETupleT e1 e2 _ -> do
-        s1 <- seqExp e1 
+        s1 <- seqExp e1
         s2 <- seqExp e2
         return $ s1 ++ s2
-    EIdT _ _ _ -> halt "not yet implemented" 
+    EIdT _ _ _ -> halt "not yet implemented"
     EFunCallT id as _ -> do
         s <- seqf seqExp as
         c <- seqFunCall id
@@ -83,12 +82,12 @@ seqExp e = case e of
 seqVar :: VarId -> Sequencer
 seqVar id = do
     (_, varTab) <- getState
-    return ["ldc " ++ case M.lookup id varTab of Just x -> show x]
+    return ["ldc " ++ show (clookupe id >!> varTab)]
 
 seqFunCall :: FunId -> Sequencer
 seqFunCall id = do
     (funTab, _) <- getState
-    return ["bsr " ++ case M.lookup id funTab of Just s -> s]
+    return ["bsr " ++ (clookupe id >!> funTab)]
 
 comment :: [Instruction] -> String -> [Instruction]
 comment (i:is) c = (i ++ " ; " ++ c) : is
